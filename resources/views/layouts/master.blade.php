75342bc260d7e8dbf7282bbc800662ba545e1b7b
3A532090 <raxttc@gmail.com>
2018-11-08 21:33:50 +0800

    建立共用區塊

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
         <title>@yield('title')|MyBlog</title>
     </head>
     <body>
+    @include('layouts.partials.navigation')
         <divclass="container">
             @yield('content')
         </div>
